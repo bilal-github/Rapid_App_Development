4fc381880d9a20b2b9009481b2341cdcc7b8762e
bilal-github <57675296+bilal-github@users.noreply.github.com>
2019-12-12 10:51:10 -0700

    Validator added, can be used to validate textfields

---
+++ Validator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // for MessageBox
+
+namespace BilalAhmad_CPRG200_Lab1
+{
+    //repository of validation methods
+    public static class Validator
+    {
+        public static bool IsPresent(TextBox tb, string name)
+        {
+            bool valid = true; // tb has input
+            if (tb.Text == "")
+            {
+                valid = false;
+                MessageBox.Show(name + " is required", "Input Error");
+                tb.Focus();
+            }
+
+            return valid;
+        }//ends IsPresent method
+
+        public static bool IsInt32(TextBox tb, string name)
+        {
+            bool valid = true;
+            int val;
+
+            if(!Int32.TryParse(tb.Text, out val))
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a whole number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+
+        public static bool IsNonNegativeInt32(TextBox tb, string name)
+        {
+            bool valid = true;
+            int val;
+
+            if (!Int32.TryParse(tb.Text, out val)) //not int32
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a whole number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            else if (val < 0) // negative
+            {
+                valid = false;
+                MessageBox.Show(name + " Must be positive or zero", "Negative Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+
+        public static bool IsDouble(TextBox tb, string name)
+        {
+            bool valid = true;
+            double val;
+
+            if (!Double.TryParse(tb.Text, out val))
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+
+        
+
+        public static bool IsNonNegativeDouble(TextBox tb, string name)
+        {
+            bool valid = true;
+            double val;
+
+            if (!Double.TryParse(tb.Text, out val)) // not a double
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a whole number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            else if (val < 0) // negative
+            {
+                valid = false;
+                MessageBox.Show(name + " Must be positive or zero", "Negative Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+
+        public static bool IsDecimal(TextBox tb, string name)
+        {
+            bool valid = true;
+            decimal val;
+
+            if (!Decimal.TryParse(tb.Text, out val))
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+        public static bool IsNonNegativeDecimal(TextBox tb, string name)
+        {
+            bool valid = true;
+            decimal val;
+
+            if (!Decimal.TryParse(tb.Text, out val)) // not a double
+            {
+                valid = false;
+                MessageBox.Show(name + " must be a whole number", "Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            else if (val < 0) // negative
+            {
+                valid = false;
+                MessageBox.Show(name + " Must be positive or zero", "Negative Input Error");
+                tb.SelectAll();
+                tb.Focus();
+            }
+            return valid;
+        }
+    }//ends class
+}//ends namespace
